6e79ef9940ff14c674fe924431cdac2e7cd4839c
Zsombor Horvath <horvathzsombor2005@gmail.com>
2023-09-30 01:09:12 +0200

    Add new filename and refactor endpoint

--- routers/demo.py
+++ routers/demo.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from typing import Annotated
 
 from dependencies.file import parse_dataset, get_filename
 from common.world import World, WorldSnapshot, Tick
-
+from time import time
 from models import Demo
 
 router = APIRouter()
@@ -22,13 +22,19 @@ async def upload_demo(
         snapshot = world.tick(tick)
         snapshots.append(snapshot)
 
-    existing_demo = await Demo.filter(name=filename).first()
-    if existing_demo:
-        filename = "new_" + filename
-
     await Demo.create(
-        name=filename,
+        name=filename + "_" + str(int(time())),
         data=[snapshot.dict() for snapshot in snapshots]
     )
 
     return snapshots
+
+
+@router.get("/")
+async def get_demo():
+    try:
+        records = await Demo.all().values("id", "name")
+        return records
+
+    except Exception as e:
+        raise HTTPException(400, "Unable to retrieve records.")
